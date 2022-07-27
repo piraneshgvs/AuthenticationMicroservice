75be314ef877b2248b75dac2c764476a2ca21b4f
spran <spran@Piranesh>
2022-07-27 23:52:02 +0530

    Added eureka server

--- AuthenticationMicroservice/src/main/java/com/cognizant/authentication/AuthenticationMicroserviceApplication.java
+++ AuthenticationMicroservice/src/main/java/com/cognizant/authentication/AuthenticationMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.cognizant.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AuthenticationMicroserviceApplication {
 
 	public static void main(String[] args) {
