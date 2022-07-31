4d9dd4e10733557dae0b95da0755740b99d9654b
spran <spran@Piranesh>
2022-07-31 15:27:45 +0530

    Added feign client

--- AuthenticationMicroservice/src/main/java/com/cognizant/authentication/AuthenticationMicroserviceApplication.java
+++ AuthenticationMicroservice/src/main/java/com/cognizant/authentication/AuthenticationMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.cognizant.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableFeignClients
 public class AuthenticationMicroserviceApplication {
 
 	public static void main(String[] args) {
